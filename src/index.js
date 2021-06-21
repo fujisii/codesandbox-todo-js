a02662ec13398e35c111756d3e691c2f7060d1fc
fujisii <51264835+fujisii@users.noreply.github.com>
2021-06-21 21:43:04 +0900

    35. タスクの完了機能

--- src/index.js
+++ src/index.js
@@ -20,7 +20,37 @@ const onClickAdd = () => {
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
+    // 押された完了ボタンの親タグを未完了リストから削除
+    deleteFromIncompleteList(completeButton.closest("li"));
+
+    // 完了リストに追加する要素
+    const addTarget = completeButton.closest("li");
+
+    // TODO内容テキストを取得
+    const text = addTarget.querySelector("p").innerText;
+
+    // li以下を初期化
+    addTarget.textContent = null;
+
+    // divタグ生成
+    const div = document.createElement("div");
+    div.className = "list-row";
+
+    // pタグ生成
+    const p = document.createElement("p");
+    p.innerText = text;
+
+    // buttonタグ生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+
+    // liタグの子要素に各要素を設定
+    div.appendChild(p);
+    div.appendChild(backButton);
+    addTarget.appendChild(div);
+
+    // 完了リストに追加
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   // button（削除）タグ生成
@@ -28,8 +58,7 @@ const onClickAdd = () => {
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
     // 押された削除ボタンの親タグを未完了リストから削除
-    const deleteTarget = deleteButton.closest("li");
-    document.getElementById("incomplete-list").removeChild(deleteTarget);
+    deleteFromIncompleteList(deleteButton.closest("li"));
   });
 
   // 子要素を設定
@@ -37,6 +66,11 @@ const onClickAdd = () => {
   div.appendChild(completeButton);
   div.appendChild(deleteButton);
   li.appendChild(div);
+
+  // 未完了リストから指定の要素を削除
+  const deleteFromIncompleteList = (target) => {
+    document.getElementById("incomplete-list").removeChild(target);
+  };
 
   // 未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(li);
